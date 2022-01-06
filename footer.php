c415ae191d2d4132d782f439a1ed2cc80bd3db17
Taapedza Makwarimba <91124736+taapedzamakwarimba05@users.noreply.github.com>
2022-01-06 13:01:27 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+    <div class="bottom_menu"><a href="index.php">Home Page</a>  |  <a href="aboutus.php">About Us</a>  |  <a href="specialdeals.php">Special Deals</a>  |  <a href="foodzone.php">Food Zone</a>
+  | <a href="admin/index.php" target="_blank">Administrator</a> |</div>
+  <?php
+  error_reporting();
+  ?>
+  
+  <div class="bottom_addr">&copy; <?php echo date('Y'); ?> <?php echo APP_NAME; ?>. All Rights Reserved</div>
+</div>
